52c3cf1f7e2b074f742b07276441fb05dd0073b5
Julen <julenz485@gmail.com>
2018-02-14 10:26:00 +0100

    asdasd

--- app/Http/Controllers/GestionarUsuariosController.php
+++ app/Http/Controllers/GestionarUsuariosController.php
@@ -24,7 +24,7 @@ class GestionarUsuariosController extends Controller
     {
         //funcion que edita usuarios
         $data = User::find($id);
-        return view('admin/admin.EditarUsuarios', compact('data'));
+        return view('admin.EditarUsuarios', compact('data'));
     }
     public function update(Request $request, $id)
     {
